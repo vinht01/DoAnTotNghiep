14849563338ee801dacf6feff3458c138f69040b
vinht01 <vinht01>
2019-04-28 18:53:21 +0700

    lession7: Create solution

---
+++ ShopBanGiay.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShopBanGiay.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
